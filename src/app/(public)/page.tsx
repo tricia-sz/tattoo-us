dda7e21b3f0bfe52db5bb1ba18d46a679fbca25a
Patricia Souza <patricia.sz.br@gmail.com>
2025-08-03 15:23:48 -0300

    Hero Component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,15 @@
 import { Header } from "./_components/header";
+import { Hero } from './_components/hero'
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+
+      <div className="">
+        <Hero />
+      </div>
+     
     </div>
-  );
+  )
 }
